8800af0877fd73e56ecc4e8ff5b8e6caa198c89a
Fazna <faznaharees@cet.ac.in>
2020-07-20 21:55:44 +0530

    m

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Home from './components/Home'
 import Input20 from './components/Inputs/Input20'
 import Input1 from './components/Inputs/Input1'
 import Input2 from './components/Inputs/Input2'
-import Input3 from './components/Inputs/Input2'
+import Input3 from './components/Inputs/Input3'
+import Input4 from './components/Inputs/Input4'
+
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={Home}/>
+        <Route path='/home' component={Home}/>
         <Route path="/1" component={Input1}/>
         <Route path="/2" component={Input2}/>
         <Route path="/3" component={Input3}/>
+        <Route path="/4" component={Input4}/>
 
         <Route path="/20" component={Input20}/>
       </Switch>
---
+++ src/components/Inputs/Input4.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import { IonCard, IonCardContent } from '@ionic/react'
+
+export default class Input2 extends Component {
+    state = {
+        range: 3,
+        result:0,
+       
+    }
+handleSelect = (e) => {
+this.setState({
+    [e.target.name]:e.target.value
+
+})  
+  }
+
+handleChange = (e) => {
+    this.setState({
+        [e.target.name]:Number(e.target.value)
+
+    },
+    this.calculate)  
+    console.log(e.target.value)
+
+  }
+
+    calculate = () => {
+        const {range} = this.state;
+        this.setState({
+          result : Math.round((range-3)*100/12,2)
+        })   
+    }
+    render() {
+        const {range,result} = this.state;
+        return (
+            <center>
+                 <IonCard className="inputcard">
+                     <IonCardContent>
+                        <div className="row">
+                            <div className="col-md-1" style={{margin:"auto",lineHeight:"4px"}} > 
+                            {range}mA
+                            </div>
+                            <div class="col-md-10"><input type="range" name="range" min="3" max="15" value={range} class="slider" id="myRange" onChange={this.handleChange}></input>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-10" style={{margin:"auto",lineHeight:"4px"}} > 
+                           <h2 >{result} Percentage</h2>
+                            </div>
+                            {/* <div class="col-md-10"><input type="range" name="result" min="0" max="100" value={result} class="slider" id="myRange"/> */}
+                            {/* </div> */}
+                        </div>
+                     </IonCardContent>
+                 </IonCard>
+            </center>
+        )
+    }
+}
